11d21cc9fdf0a313d50d8877b85bf97bd9b0458f
marcos <viniciuspop1@hotmail.com>
2026-05-06 18:09:54 -0300

    finalmente

--- yt-player-frontend/src/components/ui/sidebar.tsx
+++ yt-player-frontend/src/components/ui/sidebar.tsx
@@ -137,9 +137,9 @@ function SidebarProvider({
           } as React.CSSProperties
         }
         className={cn(
-          "group/sidebar-wrapper flex min-h-svh w-full has-data-[variant=inset]:bg-sidebar",
-          className
-        )}
+        "group/sidebar-wrapper flex min-h-svh w-full has-data-[variant=inset]:bg-sidebar",
+        className ?? ""
+      )}
         {...props}
       >
         {children}
